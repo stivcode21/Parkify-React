7508c702528780fdc64d334b118943482413979f
stivcode21 <stivenelcodificador@gmail.com>
2025-07-15 22:03:56 -0500

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import Dashboard from "@/pages/dashboard/Dashboard";
-import UserLogin from "@/pages/userLogin/UserLogin";
-import WelcomePage from "@/pages/welcomePage/WelcomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppRouter from "./routing/AppRouter";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<UserLogin />} />
-        <Route path="/hero" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRouter />;
 }
 
 export default App;
