16c4968e0c3eae6739f48851ceecb29b9fd7f5b0
stivcode21 <stivenelcodificador@gmail.com>
2025-07-17 23:41:15 -0500

    feat: new page UserSignIn

---
+++ src/pages/userSignIn/UserSignIn.jsx
@@ -0,0 +1,12 @@
+import LayoutLogin from "../../components/templates/layoutLogin/LayoutLogin";
+import SignIn from "../../components/organisms/signIn/SignIn";
+
+const UserSignIn = () => {
+  return (
+    <LayoutLogin>
+      <SignIn />
+    </LayoutLogin>
+  );
+};
+
+export default UserSignIn;
