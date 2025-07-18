dfb71f7498f9e348769395373720bc77b0325428
stivcode21 <stivenelcodificador@gmail.com>
2025-07-17 23:41:49 -0500

    feat: organism create ProfileCreate

---
+++ src/components/organisms/profileCreate/ProfileCreate.jsx
@@ -0,0 +1,49 @@
+import styles from "./ProfileCreate.module.css";
+import Button from "@/components/templates/button/Button";
+
+const ProfileCreate = () => {
+  return (
+    <>
+      <h1 className={styles.title}>CREAR PERFIL</h1>
+      <form className={styles.form}>
+        <label className={styles.label} for="usuario">
+          Tu nombre de usuario es:{" "}
+          <span className={styles.strong}>your_name</span>
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          id="usuario"
+          name="usuario"
+          autocomplete="username"
+        />
+
+        <label className={styles.label} for="contrasena">
+          Contraseña:
+        </label>
+        <input
+          className={styles.input}
+          type="password"
+          id="contrasena"
+          name="contrasena"
+        />
+
+        <label className={styles.label} for="contrasena">
+          Confirma Contraseña:
+        </label>
+        <input
+          className={styles.input}
+          type="password"
+          id="contrasena"
+          name="contrasena"
+        />
+
+        <div className={styles.button}>
+          <Button name="Acceder" />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default ProfileCreate;
