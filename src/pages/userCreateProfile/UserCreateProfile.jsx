d2bec84be38d5c429d8acf5b1477c7d33cefde06
stivcode21 <stivenelcodificador@gmail.com>
2025-07-17 23:40:49 -0500

    feat: new page UserCreateProfile

---
+++ src/pages/userCreateProfile/UserCreateProfile.jsx
@@ -0,0 +1,12 @@
+import LayoutLogin from "../../components/templates/layoutLogin/LayoutLogin";
+import ProfileCreate from "../../components/organisms/profileCreate/ProfileCreate";
+
+const UserCreateProfile = () => {
+  return (
+    <LayoutLogin>
+      <ProfileCreate />
+    </LayoutLogin>
+  );
+};
+
+export default UserCreateProfile;
