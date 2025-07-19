e3f0fcaa9e9620c0006d88bc42ef2850db3c1b91
stivcode21 <stivenelcodificador@gmail.com>
2025-07-19 13:21:33 -0500

    Refactor: SignUp now features formValidation and subtitle

--- src/components/organisms/signUp/SignUp.jsx
+++ src/components/organisms/signUp/SignUp.jsx
@@ -2,24 +2,39 @@ import Button from "@/components/templates/button/Button";
 import styles from "./SignUp.module.css";
 import { useState } from "react";
 import { useNotification } from "@/components/templates/notificationProvider/notificationProvider";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [correo, setCorreo] = useState("");
   const notify = useNotification();
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const validateForm = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     //validacion para no enviar vacio
     if (!correo.trim()) {
-      notify("Warning", "Por favor, ingresa un correo.");
-      return;
+      notify("Warning", "Por favor, ingresa tu correo.");
+      return false;
     }
+
+    if (!emailRegex.test(correo)) {
+      notify("Warning", "El correo no cumple con el formato requerido");
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
 
     try {
       console.log(correo);
       setCorreo("");
       notify("Success", "Correo enviado correctamente");
+      navigate("/createProfile");
     } catch (error) {
       notify("Error", "Error al enviar el correo, intente nuevamente.");
     }
@@ -27,7 +42,13 @@ const SignUp = () => {
 
   return (
     <>
-      <h1 className={styles.title}>CREAR PERFIL</h1>
+      <h1 className={styles.title}>REGISTRARSE</h1>{" "}
+      <h2 className={styles.subtitle}>
+        Para registrarte, primero debes{" "}
+        <strong>autenticar tu correo electrónico.</strong> Te llegara un{" "}
+        <strong>enlace de verificación,</strong> sigue las instrucciones para
+        continuar con el registro.
+      </h2>
       <form className={styles.form} onSubmit={handleSubmit}>
         <label className={styles.label}>Correo:</label>
         <input
