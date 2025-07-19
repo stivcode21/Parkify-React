731fdd0bde585db9afd8bfd8ffbce07178e76b66
stivcode21 <stivenelcodificador@gmail.com>
2025-07-18 23:15:22 -0500

    feat: all app usage Provider Notification

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -3,6 +3,7 @@ import WelcomePage from "@/pages/welcomePage/WelcomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserSignIn from "../pages/userSignIn/UserSignIn";
 import UserCreateProfile from "../pages/userCreateProfile/UserCreateProfile";
+import UserSignUp from "../pages/userSignUp/UserSignUp";
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/signIn" element={<UserSignIn />} />
+        <Route path="/signUp" element={<UserSignUp />} />
         <Route path="/createProfile" element={<UserCreateProfile />} />
         <Route path="/hero" element={<Dashboard />} />
       </Routes>
