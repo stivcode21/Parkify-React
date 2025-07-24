c60619f87606b630c4fe1907923ec8f45ab185e2
stivcode21 <stivenelcodificador@gmail.com>
2025-07-23 22:41:45 -0500

    feat: browserRouter add in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routing/AppRouter";
 import { NotificationProvider } from "@/components/templates/notificationProvider/notificationProvider";
 
 function App() {
   return (
-    <NotificationProvider>
-      <AppRouter />
-    </NotificationProvider>
+    <BrowserRouter>
+      <NotificationProvider>
+        <AppRouter />
+      </NotificationProvider>
+    </BrowserRouter>
   );
 }
 
