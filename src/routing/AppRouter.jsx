5337d39c20201c665c80a6b5a0917e51fcadc19a
stivcode21 <stivenelcodificador@gmail.com>
2025-07-23 22:42:22 -0500

    feat: app router settings

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,21 +1,33 @@
 import Dashboard from "@/pages/dashboard/Dashboard";
 import WelcomePage from "@/pages/welcomePage/WelcomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import UserSignIn from "../pages/userSignIn/UserSignIn";
 import UserCreateProfile from "../pages/userCreateProfile/UserCreateProfile";
 import UserSignUp from "../pages/userSignUp/UserSignUp";
+import { useEffect } from "react";
+import { supabase } from "../supabase/supabase";
 
 const AppRouter = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (!session) {
+        navigate("/");
+      } else {
+        navigate("/createProfile");
+      }
+    });
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/signIn" element={<UserSignIn />} />
-        <Route path="/signUp" element={<UserSignUp />} />
-        <Route path="/createProfile" element={<UserCreateProfile />} />
-        <Route path="/hero" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/signIn" element={<UserSignIn />} />
+      <Route path="/signUp" element={<UserSignUp />} />
+      <Route path="/createProfile" element={<UserCreateProfile />} />
+      <Route path="/hero" element={<Dashboard />} />
+    </Routes>
   );
 };
 
