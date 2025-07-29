197b0df989b53fe6685731041d41be4d363ccf80
stivcode21 <stivenelcodificador@gmail.com>
2025-07-29 15:01:07 -0500

    feat: Add auth session redirect

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,8 +4,19 @@ import Reloj from "@/components/molecules/reloj/Reloj";
 import ActuallyDate from "@/components/molecules/actuallyDate/ActuallyDate";
 import ModalOverlay from "@/components/templates/modalOverlay/ModalOverlay";
 import BoxButtons from "@/components/molecules/boxButtons/BoxButtons";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { supabase } from "@/supabase/supabase";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!supabase.auth.getSession) {
+      navigate("/signIn");
+    }
+  }, []);
+
   return (
     <div className={styles.bg}>
       <div className={styles.container}>
