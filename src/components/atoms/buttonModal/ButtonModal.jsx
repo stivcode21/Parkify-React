9f96e8964054437afb254cf093726b685833e6b3
stivcode21 <stivenelcodificador@gmail.com>
2025-07-28 22:15:28 -0500

    refactor: buttonModal actually for buttonExit async Function

--- src/components/atoms/buttonModal/ButtonModal.jsx
+++ src/components/atoms/buttonModal/ButtonModal.jsx
@@ -1,17 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { supabase } from "@/supabase/supabase";
 import styles from "./ButtonModal.module.css";
 import useModalStore from "@/store/ModalStore";
+import { useNotification } from "@/components/templates/notificationProvider/notificationProvider";
 
 const ButtonModal = ({ exit = false, name, component }) => {
   const { setModalContent } = useModalStore();
+  const navigate = useNavigate();
+  const notify = useNotification();
 
-  const handleClick = () => {
+  const handleClick = async () => {
     if (exit) {
-      window.location.href = "/login";
+      const { error } = await supabase.auth.signOut();
+
+      if (error) {
+        notify("Error", "No se pudo cerrar sesión.");
+        console.log(error);
+      } else {
+        notify("Success", "Sesión cerrada con éxito.");
+        navigate("/");
+      }
     } else {
-      setModalContent(component); // pasamos el componente como contenido del modal
+      setModalContent(component);
     }
   };
-
   return (
     <button
       onClick={handleClick}
