a727bf3e27faead0e6c8ce60d0d272fb94365d97
stivcode21 <stivenelcodificador@gmail.com>
2025-07-30 22:47:21 -0500

    feat: small change in animation

--- src/components/templates/modalOverlay/ModalOverlay.jsx
+++ src/components/templates/modalOverlay/ModalOverlay.jsx
@@ -14,8 +14,8 @@ const ModalOverlay = () => {
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
-          duration: 0.4,
-          delay: 0.05,
+          duration: 0.3,
+          delay: 0.04,
           ease: [0.215, 0.61, 0.355, 1],
         }}
         className={styles.modal}
