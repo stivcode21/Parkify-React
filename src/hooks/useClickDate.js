210fd98e2781d6102a42034f179a3602926b44d8
stivcode21 <stivenelcodificador@gmail.com>
2025-08-03 20:55:33 -0500

    refactor: actually useClickDate

--- src/hooks/useClickDate.js
+++ src/hooks/useClickDate.js
@@ -1,27 +1,18 @@
-import { useState } from "react";
-
 const useClickDateTime = () => {
-  const [dateTime, setDateTime] = useState("");
-
-  const handleClick = () => {
+  const captureDate = () => {
     const now = new Date();
 
-    // Formatear la fecha
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, "0");
     const day = String(now.getDate()).padStart(2, "0");
 
-    // Formatear la hora
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
 
-    // Crear el string en el formato deseado
-    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}`;
-
-    setDateTime(formattedDateTime);
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
   };
 
-  return [dateTime, handleClick];
+  return captureDate;
 };
 
 export default useClickDateTime;
