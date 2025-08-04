f0a027ee1e903eb1addd0dee6b7ed5370f658492
stivcode21 <stivenelcodificador@gmail.com>
2025-08-03 23:20:55 -0500

    refactor: vehicleEntry and VehicleExit fully functional

--- src/components/organisms/vehicleEntry/VehicleEntry.jsx
+++ src/components/organisms/vehicleEntry/VehicleEntry.jsx
@@ -1,18 +1,117 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./VehicleEntry.module.css";
 import ParkifyLogo from "@/components/atoms/parkifyLogo/ParkifyLogo";
 import ButtonSend from "@/components/atoms/buttonSend/ButtonSend";
+import { useNotification } from "@/components/templates/notificationProvider/notificationProvider";
+import { supabase } from "@/supabase/supabase";
+import useClickDateTime from "@/hooks/useClickDate";
 
 const VehicleEntry = () => {
-  const [vehiculo, setVehiculo] = useState("moto");
+  const [vehiculo, setVehiculo] = useState("");
+  const [placa, setPlaca] = useState("");
+  const [casillero, setCasillero] = useState("");
+  const notify = useNotification();
+  const captureDate = useClickDateTime();
 
-  const handleChange = (e) => {
-    setVehiculo(e.target.value);
+  const validateForm = () => {
+    const inputPlaca = document.getElementById("placa");
+    const inputCasillero = document.getElementById("casillero");
+    inputPlaca.maxLength = "6";
+    inputCasillero.maxLength = "2";
+
+    //validacion para no enviar vacio
+    if (!vehiculo.trim()) {
+      notify("Warning", "Por favor, selecciona tipo de vehiculo.");
+      return false;
+    }
+
+    if (placa.length < 6) {
+      notify("Warning", "Por favor, ingresa una placa correcta.");
+      return false;
+    }
+
+    if (!placa.trim()) {
+      notify("Warning", "Por favor, ingresa la placa del vehículo.");
+      return false;
+    }
+
+    return true;
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Vehículo seleccionado:", vehiculo);
+    if (!validateForm()) return;
+    const fecha = captureDate();
+
+    try {
+      // Obtener el usuario autenticado
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+
+      if (userError || !user) {
+        notify("Error", "No se pudo obtener el usuario autenticado.");
+        return;
+      }
+
+      // Insertar en la tabla 'vehiculos'
+      const { error: insertError } = await supabase.from("vehiculo").insert({
+        placa: placa.toUpperCase(),
+        tipo: vehiculo,
+        fecha_entrada: fecha,
+        id_user: user.id,
+      });
+
+      //validar si el casillero existe y esta libre solo si se ingreso un casillero
+      if (casillero.trim()) {
+        const { data } = await supabase
+          .from("casilleros")
+          .select("*")
+          .eq("id_locker", casillero)
+          .eq("estado", false)
+          .eq("id_user", user.id)
+          .single();
+
+        // Si el casillero existe y está libre, actualizarlo
+        if (data) {
+          const { error: lockerError } = await supabase
+            .from("casilleros")
+            .update({
+              placa: placa.toUpperCase(),
+              estado: true,
+            })
+            .eq("id_locker", casillero)
+            .eq("id_user", user.id);
+
+          if (lockerError) {
+            console.error("Error al actualizar el casillero:", lockerError);
+          } else {
+            notify("Success", "Casillero asignado correctamente.");
+          }
+        } else {
+          notify("Info", "Casillero ya esta ocupado.");
+          return;
+        }
+      }
+
+      if (insertError) {
+        notify(
+          "Warning",
+          "Ocurrio un problema al ingresar el vehiculo, por favor intente nuevamente."
+        );
+        console.log(insertError);
+        return;
+      }
+
+      setPlaca("");
+      setVehiculo("");
+      setCasillero("");
+      notify("Success", "Vehiculo ingresado correctamente.");
+    } catch (error) {
+      notify("Error", "Error al guardar los datos, intente nuevamente.");
+      console.log(error);
+    }
   };
 
   return (
@@ -27,7 +126,7 @@ const VehicleEntry = () => {
               name="vehiculo"
               value="moto"
               checked={vehiculo === "moto"}
-              onChange={handleChange}
+              onChange={(e) => setVehiculo(e.target.value)}
             />
             <span>Moto</span>
           </label>
@@ -38,7 +137,7 @@ const VehicleEntry = () => {
               name="vehiculo"
               value="carro"
               checked={vehiculo === "carro"}
-              onChange={handleChange}
+              onChange={(e) => setVehiculo(e.target.value)}
             />
             <span>Carro</span>
           </label>
@@ -46,17 +145,28 @@ const VehicleEntry = () => {
 
         <div className={styles.containerPlaca}>
           <span className={styles.labelPlaca}>Placa</span>
-          <input className={styles.input} type="text" />
+          <input
+            className={styles.input}
+            type="text"
+            value={placa}
+            maxLength={6}
+            onChange={(e) => setPlaca(e.target.value)}
+            id="placa"
+          />
         </div>
 
         <div className={styles.containerLocker}>
           <input
             className={`${styles.input} ${styles.inputLocker}`}
             type="text"
+            value={casillero}
+            onChange={(e) => setCasillero(e.target.value)}
+            id="casillero"
+            maxLength={2}
           />
           <span className={styles.labelLocker}>N° Casillero</span>
         </div>
-        <ButtonSend name="Ingresar" />
+        <ButtonSend name="Ingresar" onClick={handleSubmit} />
       </div>
     </>
   );
--- src/components/organisms/vehicleExit/VehicleExit.jsx
+++ src/components/organisms/vehicleExit/VehicleExit.jsx
@@ -1,20 +1,155 @@
-import React from "react";
 import styles from "./VehicleExit.module.css";
 import ParkifyLogo from "@/components/atoms/parkifyLogo/ParkifyLogo";
 import ButtonSend from "@/components/atoms/buttonSend/ButtonSend";
+import { useNotification } from "@/components/templates/notificationProvider/notificationProvider";
+import { supabase } from "@/supabase/supabase";
+import useClickDateTime from "@/hooks/useClickDate";
+import useCalculoPago from "@/hooks/useCalculoPago";
+import TicketBill from "@/components/molecules/ticketBill/TicketBill";
+import useTiempoTranscurrido from "@/hooks/useTiempoTranscurrido";
+import { useState } from "react";
 
 const VehicleExit = () => {
+  const [dataVehiculo, setDataVehiculo] = useState(null);
+  const [placa, setPlaca] = useState("");
+  const [ticketBill, setTicketBill] = useState(false);
+  const notify = useNotification();
+  const captureDate = useClickDateTime();
+
+  const fechaActual = captureDate();
+  const valorAPagar = useCalculoPago(dataVehiculo?.fecha_entrada);
+  const tiempoPasado = useTiempoTranscurrido(dataVehiculo?.fecha_entrada);
+
+  const validateForm = () => {
+    if (placa.trim().length !== 6) {
+      notify("Warning", "Por favor, ingresa una placa válida de 6 caracteres.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    try {
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+
+      if (userError || !user) {
+        notify("Error", "No se pudo obtener el usuario autenticado.");
+        return;
+      }
+
+      const { data: dataVehicle, error } = await supabase
+        .from("vehiculo")
+        .select("*")
+        .eq("placa", placa.toUpperCase())
+        .eq("id_user", user.id)
+        .single();
+
+      if (error || !dataVehicle) {
+        notify("Warning", "Vehículo no encontrado.");
+        return;
+      }
+
+      setDataVehiculo(dataVehicle);
+      setTicketBill(true);
+      setPlaca("");
+    } catch (err) {
+      console.error(err);
+      notify("Error", "Error al buscar el vehículo.");
+    }
+  };
+
+  const handleExitVehicle = async () => {
+    const {
+      data: { user },
+      error: userError,
+    } = await supabase.auth.getUser();
+
+    if (userError || !user) {
+      notify("Error", "No se pudo obtener el usuario autenticado.");
+      return;
+    }
+
+    try {
+      //  Insertar en historial
+      const { error: insertError } = await supabase.from("historial").insert({
+        placa: dataVehiculo.placa,
+        tipo: dataVehiculo.tipo,
+        fechaEntrada: dataVehiculo.fecha_entrada,
+        fechaSalida: fechaActual,
+        pago: valorAPagar,
+        id_user: user.id,
+      });
+
+      if (insertError) throw insertError;
+
+      // Liberar casillero
+      const { error: lockerError } = await supabase
+        .from("casilleros")
+        .update({
+          placa: null,
+          estado: false,
+        })
+        .eq("placa", dataVehiculo.placa)
+        .eq("id_user", user.id);
+
+      if (lockerError) throw lockerError;
+
+      // Eliminar vehículo
+      const { error: deleteError } = await supabase
+        .from("vehiculo")
+        .delete()
+        .eq("placa", dataVehiculo.placa)
+        .eq("id_user", user.id);
+
+      if (deleteError) throw deleteError;
+
+      notify("Success", "Vehículo dado de salida correctamente.");
+      setDataVehiculo(null);
+      setTicketBill(false);
+    } catch (error) {
+      console.error("Error al procesar la salida:", error);
+      notify("Error", "Ocurrió un error al procesar la salida.");
+    }
+  };
+
   return (
     <>
       <ParkifyLogo />
       <h2 className={styles.title}>Salida de Vehículos</h2>
       <div className={styles.container}>
-        <div className={styles.containerPlaca}>
-          <span className={styles.labelPlaca}>Placa</span>
-          <input className={styles.input} type="text" />
-        </div>
-
-        <ButtonSend name="Buscar" />
+        {ticketBill ? (
+          <div className={styles.column1}>
+            <TicketBill
+              selected={dataVehiculo?.placa}
+              tiempoPasado={tiempoPasado}
+              valorAPagar={valorAPagar}
+            />
+            <div className={styles.containerButton}>
+              <ButtonSend name="Salida" onClick={handleExitVehicle} />
+            </div>
+          </div>
+        ) : (
+          <div className={styles.column2}>
+            <div className={styles.containerPlaca}>
+              <span className={styles.labelPlaca}>Placa</span>
+              <input
+                className={styles.input}
+                type="text"
+                value={placa}
+                onChange={(e) => setPlaca(e.target.value)}
+                maxLength={6}
+                id="placa"
+              />
+            </div>
+            <ButtonSend name="Buscar" onClick={handleSubmit} />
+          </div>
+        )}
       </div>
     </>
   );
