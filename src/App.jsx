fa040cf9b6ec3a4daece30c527462764ac169c87
stivcode21 <stivenelcodificador@gmail.com>
2025-08-05 10:43:48 -0500

    feat: import loaderProvider in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routing/AppRouter";
 import { NotificationProvider } from "@/components/templates/notificationProvider/notificationProvider";
+import { LoaderProvider } from "./context/loaderProvider/LoaderProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <NotificationProvider>
-        <AppRouter />
-      </NotificationProvider>
+      <LoaderProvider>
+        <NotificationProvider>
+          <AppRouter />
+        </NotificationProvider>
+      </LoaderProvider>
     </BrowserRouter>
   );
 }
