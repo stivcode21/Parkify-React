6275db26bdac8fd657bb875c896de8f780f314e4
stivcode21 <stivenelcodificador@gmail.com>
2025-08-29 22:59:02 -0500

    feat: new component MainLogoParkify

---
+++ src/components/atoms/mainParkifyLogo/MainParkifyLogo.jsx
@@ -0,0 +1,11 @@
+import styles from "./MainParkifyLogo.module.css";
+
+const MainParkifyLogo = () => {
+  return (
+    <h1 className={styles.title}>
+      Parki<span className={styles.strong}>fy</span>
+    </h1>
+  );
+};
+
+export default MainParkifyLogo;
