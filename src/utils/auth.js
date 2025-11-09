a10715ce94387d77637f0e772aa57c8cf7179015
stivcode21 <stivenelcodificador@gmail.com>
2025-11-09 12:25:14 -0500

    feat: created one util to auth

---
+++ src/utils/auth.js
@@ -0,0 +1,15 @@
+export const checkAuth = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/auth/dashboard", {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (!res.ok) return false;
+
+    const data = await res.json();
+    return data?.user ? true : false;
+  } catch (error) {
+    return false;
+  }
+};
