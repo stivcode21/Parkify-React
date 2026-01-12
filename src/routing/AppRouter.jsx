04751f52f37566ba77c660e5631773959c8caa38
stivcode21 <stivenelcodificador@gmail.com>
2026-01-12 17:33:02 -0500

    feat: The logic for route control is modified

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,23 +1,46 @@
 import Dashboard from "@/pages/dashboard/Dashboard";
 import WelcomePage from "@/pages/welcomePage/WelcomePage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import UserSignIn from "../pages/userSignIn/UserSignIn";
-import UserCreateProfile from "../pages/userCreateProfile/UserCreateProfile";
-import UserSignUp from "../pages/userSignUp/UserSignUp";
 import VehicleList from "../components/organisms/vehicleList/VehicleList";
 import HistoryLog from "../components/organisms/historyLog/HistoryLog";
+import { useEffect } from "react";
+import { checkAuth } from "@/utils/auth";
+import ModalOverlay from "@/components/templates/modalOverlay/ModalOverlay";
 
 const AppRouter = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const verifySession = async () => {
+      const loggedIn = await checkAuth();
+      const isPublic = location.pathname === "/" || location.pathname === "/signIn";
+
+      if (!loggedIn) {
+        if (!isPublic) {
+          navigate("/signIn", { replace: true });
+        }
+        return;
+      }
+
+      if (isPublic) {
+        navigate("/dashboard", { replace: true });
+      }
+    };
+    verifySession();
+  }, [location.pathname, navigate]);
+
   return (
-    <Routes>
-      <Route path="/" element={<WelcomePage />} />
-      <Route path="/signIn" element={<UserSignIn />} />
-      {/* <Route path="/signUp" element={<UserSignUp />} />
-      <Route path="/createProfile" element={<UserCreateProfile />} /> */}
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/list" element={<VehicleList />} />
-      <Route path="/history" element={<HistoryLog />} />
-    </Routes>
+    <>
+      <ModalOverlay />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/signIn" element={<UserSignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/list" element={<VehicleList />} />
+        <Route path="/history" element={<HistoryLog />} />
+      </Routes>
+    </>
   );
 };
 
