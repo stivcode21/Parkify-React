af2630f388c7d98137fd581bcf5463e8009b1607
stivcode21 <stivenelcodificador@gmail.com>
2026-02-25 18:58:38 -0500

    feat: integrate Lockers component into ModalOverlay and adjust styles

--- src/components/templates/modalOverlay/ModalOverlay.jsx
+++ src/components/templates/modalOverlay/ModalOverlay.jsx
@@ -3,6 +3,7 @@ import styles from "./ModalOverlay.module.css";
 import { motion } from "framer-motion";
 import useModalStore from "@/store/ModalStore";
 import ParkifyLogov2 from "@/components/atoms/parkifyLogov2/ParkifyLogov2";
+import Lockers from "../../organisms/lockers/Lockers";
 
 const ModalOverlay = () => {
   const { modalContent, closeModal } = useModalStore();
@@ -19,7 +20,21 @@ const ModalOverlay = () => {
           delay: 0.01,
           ease: [0.215, 0.61, 0.355, 1],
         }}
+        className={`${styles.modal} ${styles.firstmodal}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <Lockers />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
         className={styles.modal}
+        transition={{
+          duration: 0.2,
+          delay: 0.01,
+          ease: [0.215, 0.61, 0.355, 1],
+        }}
         onClick={(e) => e.stopPropagation()}
       >
         <div className={styles.header}>
